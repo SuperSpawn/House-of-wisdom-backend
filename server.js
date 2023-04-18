4086cb9a11dd282978f40bb2333a8837684a203c
SuperSpawn <thearktitan@gmail.com>
2023-04-18 23:04:19 +0300

    1.5

--- server.js
+++ server.js
@@ -7,20 +7,16 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const main = async () => {
-  await connectDb();
+connectDb();
 
-  app.use(express.json());
-  app.use(cors());
-  app.use(require("./middleware/authentication"));
-  app.use("/users/", require("./routes/usersRoute"));
-  app.use("/posts/", require("./routes/postsRoute"));
-  app.use("/comments/", require("./routes/commentsRoute"));
-  app.use(require("./middleware/errorHandler"));
+app.use(express.json());
+app.use(cors());
+app.use(require("./middleware/authentication"));
+app.use("/users/", require("./routes/usersRoute"));
+app.use("/posts/", require("./routes/postsRoute"));
+app.use("/comments/", require("./routes/commentsRoute"));
+app.use(require("./middleware/errorHandler"));
 
-  app.listen(port, () => {
-    console.log(`Server listening on ${port}`);
-  });
-};
-
-main();
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
